fffecbda8b46f4994d240fd9418520dffd2fe8ba
MsEmily1020 <peace10200khs@gmail.com>
2023-10-21 01:14:45 +0900

    ✨ 로그인 기능 구현

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
-var start = document.getElementById("start")
-var login = document.getElementById("login")
+var start = document.getElementById("start");
+var login = document.getElementById("login");
 
-start.addEventListener('click', e=> {
-    location.href = "signup.html"
-})
+start.addEventListener("click", (e) => {
+  location.href = "signup.html";
+});
 
-login.addEventListener('click', e => {
-    location.href = "login.html"
-})
+login.addEventListener("click", (e) => {
+  location.href = "login.html";
+});
